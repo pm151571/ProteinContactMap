c040c7066f06c1152909eaef159883a176e1676d
Patrycja <Patrycja@LAPTOP-V7HKKNJ8>
2023-11-22 01:14:26 +0100

    .

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import numpy as np
 import pylab
 
 pdb_code = "2HHB"
-pdb_filename = "2hhb.pdb"  # Correct filename for the PDB code "1BOM"
+pdb_filename = "2hhb.pdb"
 
-def calculate_distance(a, b):
-    if 'CA' in a and 'CA' in b:
-        difference = a['CA'].coord - b['CA'].coord
-        if np.sqrt(np.sum(difference * difference)) <= 8:
-            return np.sqrt(np.sum(difference * difference))
+def calculate_distance(x, y):
+    if 'CA' in x and 'CA' in y:
+        roznica = x['CA'].coord - y['CA'].coord
+        if np.sqrt(np.sum(roznica * roznica)) <= 8:
+            return np.sqrt(np.sum(roznica * roznica))
         else:
             return None
     else:
-        # Handle the case where 'CA' atom is missing in either residue
-        return None  # Or any default value or error handling suitable for your analysis
+        return None
 
 def calculate_distance_matrix(chain):
     x = np.zeros((len(chain), len(chain)), float)
@@ -23,7 +22,7 @@ def calculate_distance_matrix(chain):
             x[row, col] = calculate_distance(residue_row, residue_col)
     return x
 
-structure = Bio.PDB.PDBParser().get_structure("chains", pdb_filename)
+structure = Bio.PDB.PDBParser().get_structure("lancuchy", pdb_filename)
 model = structure[0]
 chain_A = model["A"]
 distance_matrix = calculate_distance_matrix(chain_A)
